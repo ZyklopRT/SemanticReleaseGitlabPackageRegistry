9a8eb535eea8217f23a574e54e32fdcbdef00f7a
ZyklopRT <nomail@example.com>
2023-12-27 03:55:19 +0100

    fix: changed module.exports to esm export standard

--- index.js
+++ index.js
@@ -8,17 +8,15 @@ let verified;
  * @param {*} pluginConfig The semantic-release plugin config
  * @param {*} context The context provided by semantic-release
  */
-async function verifyConditions(pluginConfig, context) {
+export async function verifyConditions(pluginConfig, context) {
     await verify(pluginConfig, context);
     verified = true;
 }
 
-async function publish(pluginConfig, context) {
+export async function publish(pluginConfig, context) {
 
     if (!verified) {
         await verifyConditions(pluginConfig, context);
     }
     await publishComposer(pluginConfig, context);
 }
-
-module.exports = { verifyConditions, publish };
